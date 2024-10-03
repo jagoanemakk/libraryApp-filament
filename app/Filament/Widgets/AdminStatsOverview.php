e29ceaf84b5eb03ad9d8aee37878037fe466a4cf
jagoanemakk <jagoanemak1234@gmail.com>
2024-10-03 21:50:14 +0700

    feat: add admin stats to admin dashboard

---
+++ app/Filament/Widgets/AdminStatsOverview.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\User;
+use App\Models\Loans;
+use App\Models\Books;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class AdminStatsOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Total Users', User::query()->where('name', '!=', 'Admin')->count())
+                ->description('32k increase')
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->chart([7, 2, 10, 3, 15, 4, 17])
+                ->color('success'),
+            Stat::make('Total Loans', Loans::query()->count())
+                ->description('7% increase')
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->color('success'),
+            Stat::make('Books', Books::query()->count())
+                ->description('7% increase')
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->color('success'),
+        ];
+    }
+
+    public static function canView(): bool
+    {
+        return !auth()->user()->roles->pluck('name')->where('name', '!=', 'SuperAdmin')->first();
+    }
+}
